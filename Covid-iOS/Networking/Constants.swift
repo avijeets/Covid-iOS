b0cc159034de1d34e2e7905597a12b1dc3a31a5a
Avijeet Sachdev <a@avijeets.com>
2020-06-30 22:08:50 -0400

    Constants

---
+++ Covid-iOS/Networking/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Covid-iOS
+//
+//  Created by Avijeet Sachdev on 6/30/20.
+//  Copyright © 2020 Avijeet Sachdev. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static let baseURL = "https://covidtracking.com"
+    static let currentURL = "\(Constants.baseURL)/api/v1/states/current.json"
+}
