8d4ffa85b2055f25e9aa3725986780406a1b84a7
Avijeet Sachdev <a@avijeets.com>
2020-06-30 22:00:04 -0400

    Initial Commit

---
+++ Covid-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-iOS
+//
+//  Created by Avijeet Sachdev on 6/30/20.
+//  Copyright © 2020 Avijeet Sachdev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
