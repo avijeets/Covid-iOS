4ae253a4b8508ead3db5ac864395d69a40288164
Avijeet Sachdev <a@avijeets.com>
2020-07-01 00:33:28 -0400

    watchOS boilerplate

---
+++ Covid-watchOS Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Covid-watchOS Extension
+//
+//  Created by Avijeet Sachdev on 7/1/20.
+//  Copyright © 2020 Avijeet Sachdev. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
